94ede7877f90432639523a959ab4df212b58c169
Antonio Muñoz <tonicubero12@gmail.com>
2020-10-14 10:33:42 +0200

    exercise 4 added

---
+++ 1º/PR/Ejercicios/Ejercicio_Clase_4.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Ejercicio_Clase_4 {
+  
+  public static void main(String[] args) {
+    /*
+    programa que lea por teclado 3 numeros enteros H, M, S correspondientes a hora, minutos y segundos respectivamente. 
+    comprueba si la hora que indica es una hora válida.
+    Muestra si es valido o no.
+    */
+
+    int Hour = 0;
+    int Minut = 0;
+    int Seconds = 0;
+
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Please, introduce the hour... (24h format)");
+
+    Hour = scan.nextInt();
+
+    
+    System.out.println("Please, introduce the minutes...");
+
+    Minut = scan.nextInt();
+
+    
+    System.out.println("Please, introduce the seconds...");
+
+    Seconds = scan.nextInt();
+
+    if(-1 < Hour && Hour < 25){
+
+      if(-1 < Minut && Minut < 61){
+
+        if(-1 < Seconds && Seconds < 61){
+
+          System.out.println("The time is valid");
+
+        }
+      }
+    }else{
+
+      System.out.println("The time is not valid");
+
+    }
+  
+    scan.close();
+  }
+}
