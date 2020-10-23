56697a01b7eb52757c9c9aeead5df264f5c5211b
Antonio Muñoz <tonicubero12@gmail.com>
2020-10-23 16:30:26 +0200

    class work

---
+++ 1º/PR/Ejercicios_Clase/guess.java
@@ -0,0 +1,9 @@
+public class guess {
+  public static void main(String[] args) {
+    /*
+      -Crea un juego que genere un numero aleatorio y el jugador tiene 3 intentos para adivinar el número
+      
+    */
+  }
+  
+}
---
+++ 1º/PR/Ejercicios_Clase/random.java
@@ -0,0 +1,7 @@
+import java.util.Math;
+
+public class random {
+  public static void main(String[] args) { //Math.random() genera un aleatorio entre el 0 y 0,99
+    double numeroAleatorio = Math.floor(Math.random()*10+1);
+  }  
+}
