86e48552c4b2ef7002262b11ae9485bc4af35d3a
Antonio <tonicubero12@gmail.com>
2020-12-04 12:49:07 +0100

    class work

--- 1º/PR/1_Trimestre/Ejercicios_Clase/Piramides/piramide1.java
+++ 1º/PR/1_Trimestre/Ejercicios_Clase/Piramides/piramide1.java
@@ -19,11 +19,11 @@ public class piramide1{
     for (int fila = 1; fila <= alt; fila++) {
       for(int nEspacios = aux -1; nEspacios >= 0 ; nEspacios--){
         //Para los espacios
-        System.out.print(" ");
+        System.out.print("  ");
       }
       for(int nAsteriscos = 1 ; nAsteriscos <= (fila*2)-1 ; nAsteriscos++){
         //Para los asteriscos
-        System.out.print("*");   
+        System.out.print("* ");   
       }
       aux--;
       System.out.println(""); 
---
+++ 1º/PR/1_Trimestre/Ejercicios_Clase/Piramides/piramide5.java
@@ -0,0 +1,29 @@
+public class piramide5 {
+  public static void main(String[] args) {
+    int h = 6;
+    // Me permite generar las plantas
+    for (int i = 0; i < h; i++) {
+      int espacios = h - i - 1; /// igual igual que la j
+      // int asteriscos = 2 * i + 1;
+      for (int x = 0; x < espacios; x++) {
+        System.out.print("  ");
+      }
+      /**
+       * Dado el numero de asteriscos imprimir 1 121 12321
+       * 
+       * concuerda con el i+1;
+       */
+      for (int x = 1; x <= i+1; x++) {
+        System.out.print(x+" ");
+      }
+      if (i == 0) {
+      } else {
+        for (int x = i; x > 0; x--) {
+          System.out.print(x+" ");
+        }
+      }
+      System.out.println("");
+      // System.out.print("\n");
+    }
+  }
+}
