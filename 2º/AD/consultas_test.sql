e08366e85d213e359ca27de03ec81d543c2f407d
ErTonix12 <tonicubero12@gmail.com>
2021-09-21 21:27:32 +0200

    AD class work

--- 2º/AD/consultas_test.sql
+++ 2º/AD/consultas_test.sql
@@ -1,4 +1,4 @@
-----------------------------------------------------------------
+-----------------------------------------------------------------
 SELECT
     COUNT(p.cod)
 FROM
@@ -7,7 +7,8 @@ FROM
 WHERE
     prov.cif = 123456789;
 
-----------------------------------------------------------------
+-------------------------------------------------------------------
+-------------------------------------------------------------------
 SELECT
     prov.nombre AS PROVEEDOR,
     COUNT(cod) AS PRODUCTOS
@@ -19,7 +20,8 @@ WHERE
 GROUP BY
     prov.id;
 
-----------------------------------------------------------------
+-------------------------------------------------------------------
+-------------------------------------------------------------------
 SELECT
     prov.nombre AS PROVEEDOR,
     COUNT(cod) AS PRODUCTOS
@@ -29,7 +31,8 @@ FROM
 GROUP BY
     prov.id;
 
-----------------------------------------------------------------
+-------------------------------------------------------------------
+-------------------------------------------------------------------
 SELECT
     prov.nombre AS PROVEEDOR,
     COUNT(cod) AS PRODUCTOS
@@ -39,7 +42,8 @@ FROM
 GROUP BY
     prov.id;
 
-----------------------------------------------------------------
+-------------------------------------------------------------------
+-------------------------------------------------------------------
 --listado de todas las ventas quien con dni y nombre el que y a quien
 SELECT
     cl.dni,
@@ -53,7 +57,8 @@ FROM
     LEFT JOIN proveedor prov ON p.id_proveedor = prov.id
     AND c.id_producto = p.cod;
 
------------------------------------------------------------------
+-------------------------------------------------------------------
+-------------------------------------------------------------------
 --ordenar los clientes que han gastado de mas a menos
 SELECT
     c.nombre,
@@ -67,7 +72,8 @@ GROUP BY
 ORDER BY
     TOTAL ASC;
 
-------------------------------------------------------------------
+-------------------------------------------------------------------
+-------------------------------------------------------------------
 --MOSTRAR LOS NOMBRES CON MAS DE 4 LETRAS 
 SELECT
     c.nombre,
@@ -78,9 +84,48 @@ FROM
     LEFT JOIN producto AS p ON co.id_producto = p.cod
 GROUP BY
     c.id
-HAVING 
+HAVING
     LENGTH(c.nombre) > 4
 ORDER BY
     TOTAL ASC;
+-------------------------------------------------------------------
+-------------------------------------------------------------------
+SELECT
+    cliente.nombre,
+    IFNULL(SUM(producto.precio), 0) AS Total
+FROM
+    cliente
+    LEFT JOIN compra ON cliente.id = compra.id_cliente
+    LEFT JOIN producto ON compra.id_producto = producto.cod
+GROUP BY
+    cliente.nombre;
 
-------------------------------------------------------------------
+-------------------------------------------------------------------
+-------------------------------------------------------------------
+SELECT
+    cliente.dni AS dni,
+    cliente.nombre AS Cliente,
+    producto.nombre AS Producto,
+    proveedor.nombre AS Nombre_Proveedor
+FROM
+    compra
+    INNER JOIN cliente ON compra.id_cliente = cliente.id
+    INNER JOIN producto ON compra.id_producto = producto.cod
+    INNER JOIN proveedor ON producto.id_proveedor = proveedor.id;
+
+-------------------------------------------------------------------
+-------------------------------------------------------------------
+SELECT
+    prov.nombre AS Proveedor,
+    COUNT(cod) AS Productos
+FROM
+    producto AS p
+    RIGHT JOIN proveedor AS prov ON p.id_proveedor = prov.id
+GROUP BY
+    prov.id;
+
+-------------------------------------------------------------------
+-------------------------------------------------------------------
+
+--Esto simplemente es una prueba de lectura y escritura para ver si da pantallazo azul 
+--No tiene que interpretarse como nada
